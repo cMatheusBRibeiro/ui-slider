cea6b13d16f5befc5d55519f5da1dab7d05cbdaa
Matheus Ribeiro <matheus.ribeiro@fotosensores.com.br>
2021-08-25 17:58:20 -0300

    #FEAT: Iniciando documentação e exemplos

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ui-slider
+
+## Descrição 
+
+Este repositório tem como objetivo armazenar a implementação de um slider utilizando um dos conceitos das heurísticas de nielsen, sendo ela a _"Estética e design minimalista"_.
+
+## Tecnologias
+
+Para este pequeno projeto, será utilizado apenas VueJS visando implementar algo próximo da realidade e, também, facilitar na utilização de aditivos ao template. Além do VueJS, pode-se citar a utilização do SCSS.
+
+## Arquivos
+
+
+
+## Utilização
+
+
+
+## Resultados
+
+
+
+## Referências
+
+- [Heurísticas de Nielsen](https://brasil.uxdesign.cc/10-heur%C3%ADsticas-de-nielsen-para-o-design-de-interface-58d782821840)
+- [VueJS](https://vuejs.org/)
+- [SCSS](https://sass-lang.com/)
---
+++ app/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <main id="app" class="bg-light">
+    <div id="sliders" class="w-25">
+
+      <div class="form-group w-100">
+        <label for="range-1">
+          Horário selecionado: {{ tratarHorario(data) }}
+        </label>
+        <input type="range" 
+          id="range-1" class="form-control" 
+          min="0" max="1439" step="1" 
+          v-model="data">
+      </div>
+
+      <div class="form-group w-100">
+        <label for="range-1">
+          Horário selecionado: {{ tratarHorario(data) }}
+        </label>
+        <input type="range" 
+          id="range-1" class="custom-range-1"
+          min="0" max="1439" step="1" 
+          v-model="data">
+      </div>
+
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    data: 0
+  }),
+  methods: {
+    tratarHorario(horario) {
+      let horas = Math.floor(horario / 60)
+      let minutos = Math.floor(horario % 60)
+      return `${this.pad(horas)}:${this.pad(minutos)}`
+    },
+    pad(n) {
+      return (n >= 0 && n < 10) ? `0${n}` : n.toString()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+#app {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.custom-range-1 {
+  display: block;
+  width: 100%;
+}
+
+</style>
