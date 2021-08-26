0fc73e18214712fd9d1c457db7daa212cfc1097f
Matheus Ribeiro <matheus.ribeiro@fotosensores.com.br>
2021-08-26 12:14:44 -0300

    #FEAT: Adicionando um modelo de slider

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <main id="app" class="bg-light">
-    <div id="sliders" class="w-25">
+    <div id="sliders" class="w-50">
 
       <div class="form-group w-100">
         <label for="range-1">
@@ -13,13 +13,17 @@
       </div>
 
       <div class="form-group w-100">
-        <label for="range-1">
-          Horário selecionado: {{ tratarHorario(data) }}
-        </label>
-        <input type="range" 
-          id="range-1" class="custom-range-1"
-          min="0" max="1439" step="1" 
-          v-model="data">
+        <div class="custom-range-1">
+          <div class="sliderValue">
+            <span>{{ tratarHorario(data) }}</span>
+          </div>
+          <div class="field">
+            <input type="range" 
+              min="0" max="1439" step="1" 
+              v-model="data" 
+              @input="updateSliderPosition" @blur="removePointer">
+          </div>
+        </div>
       </div>
 
     </div>
@@ -40,6 +44,15 @@ export default {
     },
     pad(n) {
       return (n >= 0 && n < 10) ? `0${n}` : n.toString()
+    },
+    updateSliderPosition() {
+      const sliderValue = document.querySelector('.sliderValue span')
+      sliderValue.style.left = ((this.data / 1439) * 100) + '%';
+      sliderValue.classList.add('show')
+    },
+    removePointer() {
+      const sliderValue = document.querySelector('.sliderValue span')
+      sliderValue.classList.remove('show')
     }
   }
 }
@@ -59,6 +72,77 @@ export default {
 .custom-range-1 {
   display: block;
   width: 100%;
+  height: 80px;
+  background: #FFF;
+  border-radius: 10px;
+  text-align: center;
+  padding: 0 50px 0 50px;
+  min-width: 400px;
+
+  .sliderValue {
+    position: relative;
+    width: 100%;
+
+    span {
+      position: absolute;
+      height: 55px;
+      width: 55px;
+      color: #FFF;
+      font-weight: 500;
+      top: -40px;
+      transform: translateX(-50%) scale(0);
+      transform-origin: bottom;
+      transition: transform 0.3s ease-in-out;
+      line-height: 55px;
+      z-index: 2;
+
+      &.show {
+        transform: translateX(-50%) scale(1);
+      }
+
+      &:after {
+        position: absolute;
+        content: '';
+        height: 55px;
+        width: 55px;
+        background: #664AFF;
+        left: 50%;
+        transform: translateX(-50%) rotate(45deg);
+        border: 3px solid #FFF;
+        border-top-left-radius: 50%;
+        border-top-right-radius: 50%;
+        border-bottom-left-radius: 50%;
+        z-index: -1;
+      }
+    }
+  }
+
+  .field {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+
+    input {
+      -webkit-appearance: none;
+      height: 3px;
+      width: 100%;
+      background: #DDD;
+      border-radius: 5px;
+      outline: none;
+      border: none;
+
+      &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        height: 20px;
+        width: 20px;
+        background: #664AFF;
+        border-radius: 50%;
+        cursor: pointer;
+      }
+    }
+  }
 }
 
 </style>
